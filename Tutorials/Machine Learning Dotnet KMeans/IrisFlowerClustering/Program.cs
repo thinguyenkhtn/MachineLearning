ae54f14a663b7e9633fcd1490dda87117051d175
thinguyenkhtn <thinguyenkhtn@gmail.com>
2019-03-24 23:51:07 +0700

    new

---
+++ Tutorials/Machine Learning Dotnet KMeans/IrisFlowerClustering/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.DataView;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace IrisFlowerClustering
+{
+    class Program
+    {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
+        static void Main(string[] args)
+        {
+            var mlContext = new MLContext(seed: 0);
+            var dataView = mlContext.Data.LoadFromTextFile<IrisData>(_dataPath, hasHeader: false, separatorChar: ',');
+            string featuresColumnName = "Features";
+            var pipeline = mlContext.Transforms
+                .Concatenate(featuresColumnName, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+                .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, clustersCount: 3));
+            var model = pipeline.Fit(dataView);
+
+            using (var fileStream = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                mlContext.Model.Save(model, fileStream);
+            }
+
+            var predictor = model.CreatePredictionEngine<IrisData, ClusterPrediction>(mlContext);
+
+            var prediction = predictor.Predict(TestIrisData.Setosa);
+            Console.WriteLine($"Cluster: {prediction.PredictedClusterId}");
+            Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances)}");
+        }
+    }
+}
